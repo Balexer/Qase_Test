a464f63e88bd79d7ac055c2dbe4bf2fd347ec011
Balexer <63721661+Balexer@users.noreply.github.com>
2022-02-14 15:02:34 +0300

    Project-test: Added Constants class

---
+++ Qase_Test/Src/Constants/Constants.cs
@@ -0,0 +1,9 @@
+using Qase_Test.Core;
+
+namespace Qase_Test.Constants
+{
+    public static class Constants
+    {
+        public static readonly string HomeUrn = $"{ReadProperties.Url}projects";
+    }
+}
