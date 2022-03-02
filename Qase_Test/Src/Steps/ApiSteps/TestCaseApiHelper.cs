36448577936952787344a5f26bc017eaf3b686d8
Balexer <63721661+Balexer@users.noreply.github.com>
2022-03-02 15:02:29 +0300

    QT-5: Code refactoring

--- Qase_Test/Src/Steps/ApiSteps/TestCaseApiHelper.cs
+++ Qase_Test/Src/Steps/ApiSteps/TestCaseApiHelper.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using Newtonsoft.Json.Linq;
 using NUnit.Allure.Attributes;
+using Qase_Test.Constants;
 using Qase_Test.Core;
 using Qase_Test.Models;
 using Qase_Test.Steps.ApiSteps.Base;
@@ -10,25 +11,25 @@ namespace Qase_Test.Steps.ApiSteps
 {
     public class TestCaseApiHelper : BaseApiHelper
     {
-        private const string BaseUrl = "https://api.qase.io/v1/case";
+        private static string _baseUrl = $"{UriConstants.BaseApiUrl}case";
 
         [AllureStep("Try to create test case")]
         public static HttpStatusCode CreateTestCase(Project project, TestCase testCase, string token = null)
         {
             var parameters = $"{{\"title\":\"{testCase.CaseTitle}\"}}";
-            return Client($"{BaseUrl}/{project.ProjectCode}")
+            return Client($"{_baseUrl}/{project.ProjectCode}")
                 .Execute(BaseRequest(Method.POST, token ?? UserSettings.Token, parameters)).StatusCode;
         }
 
         [AllureStep("Try to delete test case")]
         public static HttpStatusCode DeleteTestCase(Project project, string token = null) =>
-            Client($"{BaseUrl}/{project.ProjectCode}/1")
+            Client($"{_baseUrl}/{project.ProjectCode}/1")
                 .Execute(BaseRequest(Method.DELETE, token ?? UserSettings.Token)).StatusCode;
 
         [AllureStep("Get title from test case")]
         public static string GetTestCaseTitle(Project project, string token = null)
         {
-            dynamic r = JObject.Parse(Client($"{BaseUrl}/{project.ProjectCode}/1")
+            dynamic r = JObject.Parse(Client($"{_baseUrl}/{project.ProjectCode}/1")
                 .Execute(BaseRequest(Method.GET, token ?? UserSettings.Token)).Content);
             return r.result.title;
         }
